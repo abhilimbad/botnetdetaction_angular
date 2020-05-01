350a35061511c4caa338293b9099353e96e4ec49
RuhulKH <ruhulaminkh007@gmail.com>
2020-05-01 13:17:31 +0530

    added form data

--- src/app/uploadfile/uploadfile.component.ts
+++ src/app/uploadfile/uploadfile.component.ts
@@ -16,12 +16,15 @@ fileToUpload: File = null
   }
   handleFileInput(files: FileList) {
     this.fileToUpload = files.item(0);
-    const data={
-      filedata:this.fileToUpload,
-      user:"admin"
-    }  
-    console.log(data)
-    this.http.post('url',data).subscribe((result)=>{
+    // const data={
+    //   filedata:this.fileToUpload,
+    //   user:"admin"
+    // }  
+    // console.log(data)
+    const data = new FormData()
+    data.append("file",this.fileToUpload)
+    data.append("user","admin")
+    this.http.post('http://127.0.0.1:5000/upload/',data).subscribe((result)=>{
       console.log(result)
       this.myData=result
        localStorage.setItem('result',JSON.stringify(this.myData))
